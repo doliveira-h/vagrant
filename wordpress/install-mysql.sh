a71cd8431b6d1a38403e06d00e98af93a39c7cb6
Danilo Oliveira <doliveira.h@gmail.com>
2020-06-04 17:41:07 -0300

    Added comments

--- wordpress/install-mysql.sh
+++ wordpress/install-mysql.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#load enviroment vars
+# load environment variables
 source ./env-vars.sh
 
 apt-get update
-#install mysql-server
+# install mysql-server
 apt-get -y install mysql-server 
 
-#configure mysqld.cnf to bind to all interfaces
+# configure mysqld.cnf to bind to all interfaces
 sed -i 's/bind-address.*$/bind-address = 0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
 
-#enable and restart mysql service
+# enable and restart mysql service
 systemctl enable mysql && systemctl restart mysql
 
-#create database and user
+# create database and user
 mysql -e "create database $WP_DBNAME;"
 mysql -e "create user '$WP_USER'@'%' identified by '$WP_PASSWORD';"
 mysql -e "flush privileges;"
