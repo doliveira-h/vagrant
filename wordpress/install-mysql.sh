c767beec1763d44f468a43164c7c80bc024888f2
Danilo Oliveira <doliveira.h@gmail.com>
2020-06-04 17:07:58 -0300

    Wordpress vagrantfile and install-mysql script

---
+++ wordpress/install-mysql.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+WP_DBNAME="wordpress"
+WP_USER="wordpress"
+WP_PASSWORD="wdpress#12"
+
+apt-get update
+#install mysql-server
+apt-get -y install mysql-server 
+
+#configure mysqld.cnf to bind to all interfaces
+sed -i 's/bind-address.*$/bind-address = 0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+
+#enable and restart mysql service
+systemctl enable mysql && systemctl restart mysql
+
+#create database and user
+mysql -e "create database $WP_DBNAME;"
+mysql -e "create user '$WP_USER'@'%' identified by '$WP_PASSWORD';"
+mysql -e "flush privileges;"
+mysql -e "grant all privileges on $WP_DBNAME.* to '$WP_USER'@'%';"
